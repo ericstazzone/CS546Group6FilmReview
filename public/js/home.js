6325e37fe5a15de46cab4b96967661cb3d8aa916
PatrickJuliano <pjuliano@stevens.edu>
2022-04-23 23:18:24 -0400

    Added sort option dropdown to search bar and modified routes

--- public/js/home.js
+++ public/js/home.js
@@ -1,49 +1,13 @@
-// !!! OBSOLETE !!! Will probably delete soon
+$(document).ready(function() {    
+    const sortButton = $('#sort-button');
+    const sortMenu = $('#sort-menu');
 
-// function fadeList(list, duration=250, index=0) {
-//     if (index < $(list).children().length) {
-//         $($(list).find('li').eq(index)).animate({
-//             opacity: 1,
-//             left: "+=50px"
-//         }, duration, 'easeOutQuad');
-//         setTimeout(function() {
-//             fadeList(list, duration, index + 1)
-//         }, duration/2);
-//     }
-// }
+    sortButton.text(sortMenu.children('a').first().text());
+    sortButton.val(sortMenu.children('a').first().text());
 
-// function fadeContainers(containers, duration=1000, index=0) {
-//     // This method uses simultaneous animation for the categories
-//     for (const container of $(containers).children()) {
-//         const list = $(container).find('ul').eq(0);
-//         $(container).animate({
-//             opacity: 1,
-//             bottom: '+=250px',
-//         }, duration, 'easeOutQuint');
-//         setTimeout(function() {
-//             fadeList($(list));
-//         }, duration/4);
-//     }
-
-//     // This method uses sequential animation for the categories
-//     /*
-//     if (index < $(containers).children().length) {
-//         const container = $(containers).find('section').eq(index);
-//         const list = $(container).find('ul').eq(0);
-//         $(container).animate({
-//             opacity: 1,
-//             bottom: '+=250px',
-//         }, duration, 'easeOutQuint');
-//         setTimeout(function() {
-//             fadeList($(list));
-//         }, duration/4);
-//         setTimeout(function() {
-//             fadeContainers(containers, duration, index + 1);
-//         }, duration/10);
-//     }
-//     */
-// }
-
-// $(document).ready(function() {
-//     fadeContainers($('#categories'));
-// });
+    sortMenu.children('a').click(function(event) {
+        event.preventDefault();
+        sortButton.text($(this).text());
+        sortButton.val($(this).text());
+    });
+});
--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,24 @@ const userData = data.users;
 const {ObjectId} = require('mongodb');
 
 router.get('/', async (req, res) => {
+    res.redirect('/home'); // NOTE: I often change this to test specific pages, but it should default to '/login'
+});
+
+router.get('/login', async (req, res) => {
     res.render('partials/login');
+});
+
+router.post('/login', async (req, res) => {
+    res.redirect('/home');
+});
+
+router.get('/home', async (req, res) => {
+    res.render('partials/home');
+});
+
+router.get('/logout', async (req, res) => {
+    if (req.session.username) req.session.destroy();
+    res.redirect('/login');
 });
 
 module.exports = router;
